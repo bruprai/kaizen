d5fa946ea266a9c64f7f09d6bab5ba206a4bb89d
bruprai <barry.br@tutamail.com>
2026-01-04 10:37:09 -0500

    updated app styles removed seconds from displayed time

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 export function getFormatedDate(): { day: string; date: string } {
   const newDate = new Date();
   const result = newDate.toString().split(" GMT")[0];
-
-  const dayIndex = result.indexOf(" ");
-  const day = result.substring(0, dayIndex);
-  const date = result.substring(dayIndex);
+  const dateWithoutSeconds = result.substring(0, result.length - 3);
+  const dayIndex = dateWithoutSeconds.indexOf(" ");
+  const day = dateWithoutSeconds.substring(0, dayIndex);
+  const date = dateWithoutSeconds.substring(dayIndex);
   return { day, date };
 }
